5ec7acb2148c33923f6b5ad1c14da36e8238cf69
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-05-22 08:52:13 +0200

    Add the kata for monday

---
+++ monday.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 
+ * 1/ Créer une couleur aléatoire 
+ * (Rappel : une couleur est un assemblage RGB de 3 chiffres de 0 à 255 )
+ * 
+ * 2/ Afficher en HTML 3 couleurs pour les 3 paragraphes en 
+ */
+
+ function generateColor()
+ {
+   
+ }
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <p>Enfin la voilà</p>
+    <p>Semaine tant attendu</p>
+    <p>Youpi, Symfony !</p>
+ </body>
+ </html>
