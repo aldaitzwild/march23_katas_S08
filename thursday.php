44b962ce8618df1067476a5394fa7334ed481460
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-05-25 08:50:14 +0200

    Add the kata for thursday

---
+++ thursday.php
@@ -0,0 +1,26 @@
+<?php
+    /*
+    Faire en sorte que recharger cette page fasse décrémenter le chiffre affiché de 1.
+    Une fois le chiffre arrivé à zéro, recharger la page repasse le compteur à 12
+    */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        h1 {
+            font-size: 87pt;
+            font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>12</h1>
+</body>
+</html>
